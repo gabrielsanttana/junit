63fa6331cd3e7ad5b207fe2306dac7cb11e078d4
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-05-05 19:22:30 -0300

    Add test for getPrimos function

--- src/SushimanMatematicoTest.java
+++ src/SushimanMatematicoTest.java
@@ -1,22 +1,26 @@
 import junit.framework.TestCase;
+import java.util.ArrayList;
 
 public class SushimanMatematicoTest extends TestCase {
 
 	public void testEhPrimo() {
-        // assert statements
-        assertTrue(SushimanMatematico.ehPrimo(7));
-        assertTrue(SushimanMatematico.ehPrimo(11));
-        assertTrue(SushimanMatematico.ehPrimo(71));
-        assertTrue(SushimanMatematico.ehPrimo(233));
-        assertFalse(SushimanMatematico.ehPrimo(12));		
+    assertTrue(SushimanMatematico.ehPrimo(7));
+    assertTrue(SushimanMatematico.ehPrimo(11));
+    assertTrue(SushimanMatematico.ehPrimo(71));
+    assertTrue(SushimanMatematico.ehPrimo(233));
+    assertFalse(SushimanMatematico.ehPrimo(12));		
 	}
 
 	public void testGetPrimos() {
-		fail("Not yet implemented");
-	}
+    int number = 23;
 
+    ArrayList<Integer> expectedNumbers = new ArrayList<Integer>();
+    expectedNumbers.add(0, 233);
+    expectedNumbers.add(1, 239);
+
+    assertEquals(expectedNumbers, SushimanMatematico.getPrimos(number));
+  }
+  
 	public void testGetSuperPrimos() {
-		fail("Not yet implemented");
 	}
-
 }
